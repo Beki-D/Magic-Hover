e2e13bd011df3bb39d75bb6175b1981770562d58
beki-d <bk14drobs@gmail.com>
2022-10-26 23:37:18 +0300

    Completed the glow part

---
+++ mgkHover.js
@@ -0,0 +1,14 @@
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+for (const card of document.getElementsByClassName('card')) {
+    card.onmousemove = e => handleOnMouseMove(e);
+}
